d64a987dae8cdb32b091575b1128b4847ac211a2
Student <student@avans.nl>
2024-07-02 02:18:54 -0700

    fdsa

--- src/robotics_pkg/scripts/transformatie_vision.py
+++ src/robotics_pkg/scripts/transformatie_vision.py
@@ -21,30 +21,30 @@ def lokalisatie_detectie(req):
     for _ in range(10):
         if not data.detections:
             rospy.loginfo("Geen detecties gevonden.")
-            return lokalisatieResponse(Xw=0, Yw=0, Zw=0, Naam="")
+            return lokalisatieResponse(Xw=0, Yw=0, Naam="")
 
         eerste_detectie = data.detections[0]
         eerste_resultaat_id = eerste_detectie.results[0].id
 
         if eerste_resultaat_id == 1:
             rospy.loginfo("Resultaat van de herkenning is: dop_10")
-            item = "Dop_10"
+            item = "Dop_10_0"
             naam = "Dop_10"
         elif eerste_resultaat_id == 2:
             rospy.loginfo("Resultaat van de herkenning is: Kleine Schroevendraaier")
-            item = "KleineSchroevendraaier"
+            item = "Kleine_Schroevendraaier_0"
             naam = "KleineSchroevendraaier"
         elif eerste_resultaat_id == 3:
             rospy.loginfo("Resultaat van de herkenning is: Platte kop")
-            item = "PlatteKop"
+            item = "PlatteKop_0"
             naam = "PlatteKop"
         elif eerste_resultaat_id == 4:
             rospy.loginfo("Resultaat van de herkenning is: Spanningstester")
-            item = "Spanningstester"
+            item = "Spanningstester_0"
             naam = "Spanningstester"
         else:
             rospy.logwarn("Onjuiste waarde gelezen: %i", eerste_resultaat_id)
-            return lokalisatieResponse(Xw=0, Yw=0, Zw=0, Naam="")
+            return lokalisatieResponse(Xw=0, Yw=0, Naam="")
 
         # Start waarde met een '/' als TF.
         item = "/" + item
@@ -56,7 +56,7 @@ def lokalisatie_detectie(req):
             rospy.loginfo("Translation: %s", str(trans))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
             rospy.logerr("TF fout: %s", str(e))
-            return lokalisatieResponse(Xw=0, Yw=0, Zw=0, Naam=naam)
+            return lokalisatieResponse(Xw=0, Yw=0, Naam=naam)
 
         xgem += trans[0]
         ygem += trans[1]
@@ -65,7 +65,7 @@ def lokalisatie_detectie(req):
     xgem = xgem/10
     ygem = ygem/10
     zgem = zgem/10 
-    return lokalisatieResponse(Xw=xgem, Yw=ygem, Zw=zgem, Naam=naam)
+    return lokalisatieResponse(Xw=xgem, Yw=ygem, Naam=naam)
 
 def listener_node():
     global listener, service
